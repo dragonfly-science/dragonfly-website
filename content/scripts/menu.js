32c724687f5d7c8ae086afbac2c217f27222914f
Christopher Knox <chris@dragonfly.co.nz>
2015-01-06 11:39:01 +1300

    3 moded menu

--- content/scripts/menu.js
+++ content/scripts/menu.js
@@ -1,24 +1,14 @@
 var section = 'none';
 export default function() {
-    var menu = $('#js-responsive-navigation-menu');
     var menuToggle = $('.responsive-navigation-menu-button');
-    var wrapper = $('.responsive-navigation-wrapper');
-
-    menu.removeClass("show");
+    var wrapper = document.getElementById('js-responsive-navigation-menu');
 
     $(menuToggle).on('click', function(e) {
         e.preventDefault();
-        var current = wrapper.attr('data-section');
-        if (current !== 'open') {
-            wrapper.attr('data-section', 'open');
-            section = current;
+        if(wrapper.dataset.mode  === 'open') {
+            wrapper.dataset.mode = 'closed';
         } else {
-                wrapper.attr('data-section', section);
+            wrapper.dataset.mode = 'open';
         }
-        menu.slideToggle(function(){
-            if(menu.is(':hidden')) {
-                menu.removeAttr('style');
-            }
-        });
     });
 }
