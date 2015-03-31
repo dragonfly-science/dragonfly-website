7eaf75f84d922c5987fe1fa66c342fa0583000cb
Finlay Thompson <finlay@dragonfly.co.nz>
2015-03-31 17:55:54 +1300

    Closed issues #6 and #7

--- haskell/WebSite/Collection.hs
+++ haskell/WebSite/Collection.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 module WebSite.Collection (
-    makeRules ,
+    makeRules,
+    getList, 
+    getBubbles,
     CollectionConfig(..),
     Rules()
 ) where
 
+import Data.List
 import Control.Monad (liftM)
 import Data.Monoid ((<>))
 import Data.Maybe (fromMaybe, maybeToList)
@@ -36,8 +39,9 @@ makeRules cc = do
         route $ constRoute (indexTemplate cc)
         compile $ do 
             base <- baseContext (baseName cc)
-            pages <- getPages cc
-            let  ctx = base <> pages
+            pages <- getList cc 1000
+            bubbles <- getBubbles cc Nothing
+            let  ctx = base <> pages <> bubbles
             scholmdCompiler 
                 >>= loadAndApplyTemplate (collectionTemplate cc) ctx
                 >>= loadAndApplyTemplate "templates/default.html" ctx
@@ -51,18 +55,20 @@ makeRules cc = do
     match (collectionPattern cc) $ do
         route $ setExtension "html"
         compile $ do 
+            ident <- getUnderlying
             base <- baseContext (baseName cc)
             imageMeta <- loadAll ("**/*.img.md")
-            pages <- getPages cc
-            let ctx = base <> actualbodyField "actualbody" <> pages
+            pages <- getList cc 1000
+            bubbles <- getBubbles cc (Just ident)
+            let ctx = base <> actualbodyField "actualbody" <> pages <> bubbles
             scholmdCompiler 
                 >>= loadAndApplyTemplate (pageTemplate cc) ctx
                 >>= loadAndApplyTemplate "templates/default.html" ctx
                 >>= imageCredits imageMeta
                 >>= relativizeUrls
 
-getPages :: CollectionConfig -> Compiler (Context String)
-getPages cc = do
+getList :: CollectionConfig -> Int ->  Compiler (Context String)
+getList cc limit = do
     snaps <- loadAllSnapshots (collectionPattern cc .&&. hasVersion "full") "content"
     let sortorder i = liftM (fromMaybe "666") $ getMetadataField i "sortorder"  
     snaps' <- sortItemsBy sortorder snaps
@@ -70,7 +76,28 @@ getPages cc = do
         all = cycle snaps'
         lu = [ (itemIdentifier this, (prev, next))
              | (prev, this, next) <- take l $ drop (l-1) $ zip3 all (drop 1 all) (drop 2 all) ]
-    return $ listField "pages" (pageIndexCtx lu)(return snaps')
+    return $ listField (baseName cc) (pageIndexCtx lu)(return $ take limit snaps')
+
+getBubbles :: CollectionConfig -> Maybe Identifier -> Compiler (Context String)
+getBubbles cc mident = do 
+    snaps <- loadAllSnapshots (collectionPattern cc .&&. hasVersion "full") "content"
+    let sortorder i = liftM (fromMaybe "666") $ getMetadataField i "sortorder"  
+    snaps' <- sortItemsBy sortorder snaps
+    let l = length snaps'
+        all = cycle snaps'
+        lu = [ (itemIdentifier this, (prev, next))
+             | (prev, this, next) <- take l $ drop (l-1) $ zip3 all (drop 1 all) (drop 2 all) ]
+        snaps'' = maybe (take 7 snaps') id $ do 
+                    ident <- mident
+                    let ident' = setVersion (Just "full") ident
+                    idx <- findIndex (\i -> ident' == itemIdentifier i) snaps'
+                    let (before, after) = splitAt (idx + l) (cycle snaps')
+                    return $ reverse (take 3 (reverse before)) ++ take 4 after
+        previous = listField "bubbles_prev" (pageIndexCtx lu)(return (take 3 snaps''))
+        this     = listField "bubbles_this" (pageIndexCtx lu)(return (take 1 $ drop 3 snaps''))
+        next     = listField "bubbles_next" (pageIndexCtx lu)(return (take 3 $ drop 4 snaps''))
+    return  $ previous <> this <> next
+    
 
 type PreviousNextMap = [(Identifier, (Item String, Item String))]
 pageIndexCtx :: PreviousNextMap -> Context String
--- haskell/WebSite/Work.hs
+++ haskell/WebSite/Work.hs
@@ -1,18 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 module WebSite.Work (
-    rules
+    rules, list
 ) where
 
 import WebSite.Collection
 
+config = CollectionConfig 
+       { baseName            = "work"
+       , indexTemplate       = "work/index.html"
+       , indexPattern        = "pages/work.md"
+       , collectionPattern   = "work/*.md"
+       , collectionTemplate  = "templates/post-list.html"
+       , pageTemplate        = "templates/post.html"
+       }
+
 rules :: Rules()
-rules = makeRules $ CollectionConfig 
-                        { baseName            = "work"
-                        , indexTemplate       = "work/index.html"
-                        , indexPattern        = "pages/work.md"
-                        , collectionPattern   = "work/*.md"
-                        , collectionTemplate  = "templates/post-list.html"
-                        , pageTemplate        = "templates/post.html"
-                        }
+rules = makeRules config
 
-
+list = getList config
