2b14d5879ebf52739d0ab2ea0b3d96f7c480b369
Edward Abraham <edward@dragonfly.co.nz>
2015-07-27 20:11:41 +1200

    Get publication search working (not pretty, but working)

--- content/scripts/dragonfly.js
+++ content/scripts/dragonfly.js
@@ -3,5 +3,25 @@ import menuInit from './menu';
 
 $(document).ready(function() {
     menuInit();
+    var options = {
+        valueNames: ['citation']
+    };
+    var publicationList = new List('publication-list', options);
+
+    publicationList.on('updated', function(){
+        if (publicationList.searched) {
+            $('#publication-count').show();
+            var n = publicationList.visibleItems.length;
+            if (n == 0){
+                $('#publication-count').text('Oops! There are no matching publications');
+            } else if (n == 1){
+                $('#publication-count').text('One matching publication');
+            } else {
+                $('#publication-count').text(n + ' matching publications');
+            }
+        } else {
+            $('#publication-count').hide();
+        }
+    });
 });
 
