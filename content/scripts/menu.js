b49f7bee5692c416fcd90f5ebed1ce767d3e48fb
Edward Abraham <edward@dragonfly.co.nz>
2015-09-08 20:45:03 +1200

    Make the full-screen menu disappear on click

--- content/scripts/menu.js
+++ content/scripts/menu.js
@@ -5,10 +5,17 @@ export default function() {
 
     $(menuToggle).on('click', function(e) {
         e.preventDefault();
+        e.stopPropagation();
         if(wrapper.dataset.mode  === 'open') {
             wrapper.dataset.mode = 'closed';
         } else {
             wrapper.dataset.mode = 'open';
+        }
+    });
+    
+    $(wrapper).on('click', function(e) {
+        if(wrapper.dataset.mode  === 'open') {
+            wrapper.dataset.mode = 'closed';
         }
     });
 }
