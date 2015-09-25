09166a7fe371dc85b7f56c7d6e21d173b9581f2a
Christopher Knox <chris@dragonfly.co.nz>
2015-09-25 14:52:35 +1200

    improved development workflow
    configure divshot

--- content/scripts/dragonfly.js
+++ content/scripts/dragonfly.js
@@ -1,96 +1,117 @@
-import menuInit from './menu';
-/* global $ */
++function() {
 
 
-function getParameterByName(name) {
+
+  function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
         results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
+  }
 
-function tagFilter(tags){
+  function tagFilter(tags){
     $('.tag').show().each(function(i, elem){
-        tags.split(',').forEach(function(t){
-            if (!(t === "") & (!$(elem).hasClass('tag-' + t ))){
-                $(elem).remove();
-                $("[data-tag=" + t + "]").parents(".dropdown").hide()
-            }
-        });
+      tags.split(',').forEach(function(t){
+        if (!(t === "") & (!$(elem).hasClass('tag-' + t ))){
+          $(elem).remove();
+          $("[data-tag=" + t + "]").parents(".dropdown").hide()
+        }
+      });
     });
-}
+  }
 
 
-var initPublications = function() {
+  var initPublications = function() {
     if ($("#publication-list").length === 0) {
-	return;
+      return;
     }
 
     tagFilter(getParameterByName("tag"));
 
     var options = {
-        valueNames: ['citation', 'tagslugs']
+      valueNames: ['citation', 'tagslugs']
     };
     var publicationList = new List('publication-list', options);
 
     publicationList.on('updated', function(){
-        var n = publicationList.visibleItems.length;
-        if (n == 0){
-            $('#publication-count').text('Oops! There are no matching publications');
-        } else if (n == 1){
-            $('#publication-count').text('One matching publication');
-        } else {
-            $('#publication-count').text(n + ' matching publications');
-        }
+      var n = publicationList.visibleItems.length;
+      if (n == 0){
+        $('#publication-count').text('Oops! There are no matching publications');
+      } else if (n == 1){
+        $('#publication-count').text('One matching publication');
+      } else {
+        $('#publication-count').text(n + ' matching publications');
+      }
     });
 
 
     var updateList = function() {
-        var tags = [];
-        $(".dropdown-button[data-tag]").each(function(){
-            tags.push($(this).attr("data-tag").trim());
-        });
-        tags = _.filter(tags, function(t){return !(t === "");});
-        publicationList.filter(function(item) {
-            var itemtags = _.filter(item.values().tagslugs.split(' '), function(t){return !(t === "");});
-            if (tags.length > 0){
-                return (_.difference(tags, itemtags).length === 0);
-            } else {
-                return true;
-            }
-        });
+      var tags = [];
+      $(".dropdown-button[data-tag]").each(function(){
+        tags.push($(this).attr("data-tag").trim());
+      });
+      tags = _.filter(tags, function(t){return !(t === "");});
+      publicationList.filter(function(item) {
+        var itemtags = _.filter(item.values().tagslugs.split(' '), function(t){return !(t === "");});
+        if (tags.length > 0){
+          return (_.difference(tags, itemtags).length === 0);
+        } else {
+          return true;
+        }
+      });
     }
 
     updateList();
 
     //Publication dropdown menus
     $(".dropdown-button").click(function() {
-        var $button, $menu;
-        $button = $(this);
-        $menu = $button.siblings(".dropdown-menu");
-        $menu.toggleClass("show-menu");
-        $menu.children("li").click(function() {
-            $menu.removeClass("show-menu");
-            $button.html($(this).html());
-            if ($(this)[0].hasAttribute("data-tag")) {
-                $button.attr("data-tag", $(this)[0].getAttribute("data-tag"));
-                updateList();
-            }
-        });
+      var $button, $menu;
+      $button = $(this);
+      $menu = $button.siblings(".dropdown-menu");
+      $menu.toggleClass("show-menu");
+      $menu.children("li").click(function() {
+        $menu.removeClass("show-menu");
+        $button.html($(this).html());
+        if ($(this)[0].hasAttribute("data-tag")) {
+          $button.attr("data-tag", $(this)[0].getAttribute("data-tag"));
+          updateList();
+        }
+      });
     });
-}
+  }
 
-$(document).ready(function() {
-    menuInit();
+  $(document).ready(function() {
+    var section = 'none';
+    var menuToggle = $('.responsive-navigation-menu-button');
+    var wrapper = document.getElementById('js-responsive-navigation-menu');
+
+    $(menuToggle).on('click', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      if(wrapper.dataset.mode  === 'open') {
+        wrapper.dataset.mode = 'closed';
+      } else {
+        wrapper.dataset.mode = 'open';
+      }
+    });
+
+    $(wrapper).on('click', function(e) {
+      if(wrapper.dataset.mode  === 'open') {
+        wrapper.dataset.mode = 'closed';
+      }
+    });
+
 
     initPublications();
 
     $(document).mouseup(function (e){
-        $(".dropdown-menu").each(function() {
-            if (!$(this).is(e.target)  // if the target of the click isn't the container...
-                    && $(this).has(e.target).length === 0) { // ... nor a descendant of the container
-                $(this).removeClass("show-menu");
-            }
-        });
+      $(".dropdown-menu").each(function() {
+        if (!$(this).is(e.target)  // if the target of the click isn't the container...
+          && $(this).has(e.target).length === 0) { // ... nor a descendant of the container
+            $(this).removeClass("show-menu");
+          }
+      });
     });
-});
+  });
+
+}.call(this);
