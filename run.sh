35b7889dbe54949d45943ec951cfdb2338106943
Christopher Knox <chris@dragonfly.co.nz>
2016-05-03 15:44:49 +1200

    need to create a user so that stack can use git ...

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 VERSION=20160503-2
-IMAGE=dragonfly-website:$VERSION
+IMAGE=dragonflyscience/dragonfly-website
+USERIMAGE=$USER-dragonfly-website:$VERSION
 INTERACTIVE=$([ -t 0 ] && echo '-it')
 PORT=${PORT:=8000}
 
@@ -10,9 +11,16 @@ if [ $? != 0 ]; then
     docker build -t "$IMAGE" .
 fi
 
+docker inspect $USERIMAGE >/dev/null 2>&1
+if [ $? != 0 ]; then
+    cat dockertemplate | USERID=$(id -u) GROUPID=$(id -g) envsubst | docker build -t "$USERIMAGE" -
+fi
+
+mkdir -p $HOME/.stack &&
+
 docker run --rm $INTERACTIVE -p $PORT:8000 -u $(id -u):$(id -g) \
   -w /work -v $PWD:/work \
   -v $HOME/.stack:/$HOME/.stack \
   -e STACK_ROOT=/$HOME/.stack \
-  $IMAGE ./build.sh $*
+  $USERIMAGE ./build.sh $*
 
