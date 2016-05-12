410b73803544f7f803637e12bbb2b2b89b2d5199
Christopher Knox <chris@dragonfly.co.nz>
2016-05-12 16:12:54 +1200

    fix typo

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-synced=$(aws s3 sync s3://wwwtest.dragonfly.co.nz s3://test.dragonfly.co.nz --acl public-read | sed 's/\r/\n/')
+synced=$(aws s3 sync s3://wwwtest.dragonfly.co.nz s3://www.dragonfly.co.nz --acl public-read | sed 's/\r/\n/')
 if [ "$synced" != "" ]; then 
   aws cloudfront create-invalidation --distribution-id $1 \
     --paths $(for s in $synced; do echo $s; done | grep _site | sed 's/_site//' | tr '\n' ' ' )
