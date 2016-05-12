d90b80e87dfef33134a5f926024c29a046723a25
Christopher Knox <chris@dragonfly.co.nz>
2016-05-12 16:50:01 +1200

    updated sync script to work with s3 -> s3 sync

--- sync.sh
+++ sync.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-synced=$(aws s3 sync s3://wwwtest.dragonfly.co.nz s3://www.dragonfly.co.nz --acl public-read | sed 's/\r/\n/')
+synced=$(aws s3 sync s3://wwwtest.dragonfly.co.nz s3://www.dragonfly.co.nz --acl public-read | tr -d "\r")
+
 if [ "$synced" != "" ]; then 
-  aws cloudfront create-invalidation --distribution-id $1 \
-    --paths $(for s in $synced; do echo $s; done | grep _site | sed 's/_site//' | tr '\n' ' ' )
+  echo aws cloudfront create-invalidation --distribution-id $1 \
+    --paths $(for s in $synced; do echo $s; done | grep wwwtest | sed 's/s3\:\/\/wwwtest\.dragonfly\.co\.nz//' | tr '\n' ' ' )
 fi
 
