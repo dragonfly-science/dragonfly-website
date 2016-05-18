d707caf31a8a43ef9124e919c5f93d58eb1d55a9
Christopher Knox <chris@dragonfly.co.nz>
2016-05-19 11:32:19 +1200

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@
 synced=$(aws s3 sync s3://wwwtest.dragonfly.co.nz s3://www.dragonfly.co.nz --acl public-read | tr -d "\r")
 
 if [ "$synced" != "" ]; then 
-  echo aws cloudfront create-invalidation --distribution-id $1 \
+  aws cloudfront create-invalidation --distribution-id $1 \
     --paths $(for s in $synced; do echo $s; done | grep wwwtest | sed 's/s3\:\/\/wwwtest\.dragonfly\.co\.nz//' | tr '\n' ' ' )
 fi
 
