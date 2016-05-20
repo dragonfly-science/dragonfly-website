a40e76fcb082968d9d3e9e6e3353a91412b634e4
Christopher Knox <chris@dragonfly.co.nz>
2016-05-20 17:02:55 +1200

    always invalidate / urls

--- sync.sh
+++ sync.sh
@@ -4,6 +4,7 @@ synced=$(aws s3 sync s3://wwwtest.dragonfly.co.nz s3://www.dragonfly.co.nz --acl
 
 if [ "$synced" != "" ]; then 
   aws cloudfront create-invalidation --distribution-id $1 \
-    --paths $(for s in $synced; do echo $s; done | grep wwwtest | sed 's/s3\:\/\/wwwtest\.dragonfly\.co\.nz//' | tr '\n' ' ' )
+    --paths / /work/ /people/ /news/ /data/ /publications/ \
+    $(for s in $synced; do echo $s; done | grep wwwtest | sed 's/s3\:\/\/wwwtest\.dragonfly\.co\.nz//' | tr '\n' ' ' )
 fi
 
