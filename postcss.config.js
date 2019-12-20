9a414d56fce958c62ba1081bdfbcef2925344917
simonwinter <simon@dragonfly.co.nz>
2019-12-20 16:44:46 +1300

    optimising build

--- postcss.config.js
+++ postcss.config.js
@@ -6,6 +6,16 @@ module.exports = {
         require('autoprefixer')({}),
         require('postcss-preset-env')({ browsers: 'last 2 versions' }),
         require('postcss-easings')({}),
-        require('cssnano')({})
+        require('cssnano')({}),
+        require('@fullhuman/postcss-purgecss')({
+            // Specify the paths to all of the template files in your project 
+            content: [
+                './content/templates/*.html',
+                './_site/**/*.html',
+            ],
+            
+            // Include any special characters you're using in this regular expression
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        })
     ]
 }
