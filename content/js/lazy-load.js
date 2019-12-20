da798e592ed91c09115e00c07bbccafae4181856
simonwinter <simon@dragonfly.co.nz>
2019-12-20 17:06:45 +1300

    lozad ofset update

--- content/js/lazy-load.js
+++ content/js/lazy-load.js
@@ -1,19 +1,17 @@
 import lozad from 'lozad'
 
 const LazyLoad = () => {
-    const observer = lozad({
+    const options = {
         rootMargin: '300px 0px', // syntax similar to that of CSS Margin
         threshold: 0.1 // ratio of element convergence
-    });
-    observer.observe();
+    }
+    const observer = lozad('.lozad', options)
+    observer.observe()
 
     // Lazy-load images in body copy.
-    const images = document.querySelectorAll('.body-content img');
-    const bodyObserver = lozad(images, {
-        rootMargin: '300px 0px', // syntax similar to that of CSS Margin
-        threshold: 0.1 // ratio of element convergence
-    });
-    bodyObserver.observe();
+    const images = document.querySelectorAll('.body-content img')
+    const bodyObserver = lozad(images, options)
+    bodyObserver.observe()
 }
 
 export default LazyLoad
