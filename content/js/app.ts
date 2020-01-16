086c544bc0af595d959971f2af653e4c857f1e20
simonwinter <simon@dragonfly.co.nz>
2020-01-16 13:12:19 +1300

    renamed filtering file

--- content/js/app.ts
+++ content/js/app.ts
@@ -1,10 +1,10 @@
 import smoothscroll from 'smoothscroll-polyfill'
 
 import Columns from './_2column-grid'
+import Filtering from './filtering'
 import LazyLoad from './lazy-load'
 import MobileMenu from './mobile-menu'
 import Parallaxing from './parallax'
-import Publications from './publications'
 import TopSection from './top-section'
 
 import GameOfLife from './game-of-life/game-of-life'
@@ -13,7 +13,7 @@ smoothscroll.polyfill()
 
 Zepto(($: ZeptoStatic) => {
     Columns()
-    Publications()
+    Filtering()
     MobileMenu()
     TopSection()
     LazyLoad()
