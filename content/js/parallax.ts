dd4d0a7654cffcadbab04bce8b516345fdb0049c
simonwinter <simon@dragonfly.co.nz>
2020-02-27 16:34:41 +1300

    content updates to what we do & people page

--- content/js/parallax.ts
+++ content/js/parallax.ts
@@ -2,7 +2,7 @@ import { jarallax } from 'jarallax'
 
 const Parallaxing = () => {
     jarallax(document.querySelectorAll('.jarallax'), {
-        speed: 0.05,
+        speed: 0,
         imgPosition: '50% 0',
     })
 }
