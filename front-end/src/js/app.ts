67d74c67d71d143019a1323bd01c2427ba9f5726
simonwinter <simon@dragonfly.co.nz>
2021-06-15 12:30:43 +1200

    feat(art-direction): style updates
    Updates as provided by Pep June 2021

--- front-end/src/js/app.ts
+++ front-end/src/js/app.ts
@@ -4,7 +4,7 @@ import Carousel from '~root/carousel'
 import Columns from '~root/_2column-grid'
 import Filtering from '~root/filtering'
 import Footer from '~root/footer'
-import GameOfLife from '~root/game-of-life/game-of-life'
+// import GameOfLife from '~root/game-of-life/game-of-life'
 import ImageCaptions from '~root/image-caption'
 import LazyLoad from '~root/lazy-load'
 import MobileMenu from '~root/mobile-menu'
@@ -22,6 +22,6 @@ Zepto(() => {
   ImageCaptions()
   Footer()
   StickySidebar('.sticky-container__body', '.sticky-container__element')
-  setTimeout(() => GameOfLife(), 1000)
+  // setTimeout(() => GameOfLife(), 1000)
   Carousel('carousel-images')
 })
