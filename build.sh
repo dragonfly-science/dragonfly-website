dc4f3c98fc9e57a702543058ace66068d423b4f9
simonwinter <simon@dragonfly.co.nz>
2021-07-08 16:22:12 +1200

    fix(aws): delete unecessary files on sync to S3

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ cp static-site.tgz /output
 # If AWS, sync to s3 & optionally invalidate cloudflare cache.
 # Otherwise, publish to gorbachev.
 if [[ -v AWS_ACCESS_KEY_ID && -v AWS_BUCKET && -v AWS_SECRET_ACCESS_KEY ]]; then
-  aws s3 sync _site s3://${AWS_BUCKET} --acl public-read
+  aws s3 sync _site s3://${AWS_BUCKET} --acl public-read --delete
 
   if [[ -v AWS_CLOUDFRONT_ID ]]; then
     aws cloudfront create-invalidation \
